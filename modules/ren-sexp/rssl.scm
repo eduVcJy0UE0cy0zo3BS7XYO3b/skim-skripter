deb6a20ea8ed886b3b195fced7f02d9a2e04111f
root <root@vps24672.mnogoweb.in>
2024-09-27 06:00:57 +0000

    oops add dsl itself

---
+++ modules/ren-sexp/rssl.scm
@@ -0,0 +1,56 @@
+(define-module (ren-sexp rssl)
+  #:use-module (ren-sexp scene)
+  #:export (BG BGM PAUSE JOIN CLEAN TXT JUST ->>))
+
+
+
+(define-syntax ->>
+    (syntax-rules ()
+        ; Each syntax rule is a pair: a pattern to match on, and an expression to
+        ; rewrite to. This first list is the pattern: both args (`->>`, `value`)
+        ; are just variables, which we could name however we want. Since the first
+        ; part of the pattern matches the macro name, we bind it to `->>`, but `_`
+        ; is another popular choice.
+        ; So, this pattern matches, e.g., (->> 3)...
+        ((->> value)
+            ; ...and rewrites to the expression 3, our expected result
+            value)
+
+        ; Here we match a more complex pattern, where the second var is a list--
+        ; that is, a function call. Note the ellipses, which gather multiple items
+        ; into `args` and `rest`.
+        ; This would match, e.g., (->> 3 (+ 1 2) (/ 3))...
+        ((->> value (fn args ...) rest ...)
+            ; ...and rewrite it to (->> (+ 1 2 3) (/ 3)), or (->> 6 (/ 3)).
+            ; Note that we just accomplished the "thread-last" part!
+            ; Obviously we're not at a final value yet: (->> 6 (/ 3)) will invoke
+            ; the macro again. So, we "cycle through," evaluating incrementally,
+            ; until we reach a final value.
+            (->> (fn args ... value) rest ...))
+
+        ; Finally, here we match a named function in the second position:
+        ; e.g.. (->> 3 inc (/ 3))
+        ((->> value fn rest ...)
+            ; ...and pipe it through just like we did above: (->> 4 (/ 3))
+            (->> (fn value) rest ...))))
+
+(define (BG image scenes)
+  (cons (scene-update-bg (car scenes) image) scenes))
+
+(define (BGM music scenes)
+  (cons (scene-update-music (car scenes) music) scenes))
+
+(define (PAUSE time scenes)
+  (cons (scene-update-ttl (car scenes) time) scenes))
+
+(define (JOIN sprite scenes)
+  (cons (scene-update-sprites (car scenes) sprite) scenes))
+
+(define (CLEAN scenes)
+  (cons (make-scene) scenes))
+
+(define (TXT text scenes)
+  (cons (scene-update-text (car scenes) text) scenes))
+
+(define (JUST scene scenes)
+  (cons scene scenes))
