871d7fd2b21c003f2369a6278fa8479c231af93c
Polyakov Dmitry <liltechdude@gmail.com>
2025-05-06 23:56:43 +0500

    record bug

---
+++ goblins-record-bug/game.scm
@@ -0,0 +1,14 @@
+(use-modules
+ (goblins)
+ (hoot records)
+ (ice-9 match))
+
+(define-record-type <foo>
+  (make-foo bar)
+  foo?
+  (bar foo-bar))
+
+(match (list (make-foo 1) (make-foo 2))
+  ((($ <foo> bar)
+    ($ <foo> bar*))
+   (pk (cons bar bar*))))
